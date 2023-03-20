57a95f23b5693b06ca02ece4d0f75a0d82bbe236
Belu <cha.maria.belen@gmail.com>
2023-03-20 08:36:26 -0300

    Include the .npmrc file in the build directory

--- config.rb
+++ config.rb
@@ -13,34 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+configure :build do
+  activate :asset_hash
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+  # Include the .npmrc file in the build directory
+  FileUtils.cp('.npmrc', 'build/')
+end
