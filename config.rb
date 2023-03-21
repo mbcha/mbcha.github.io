dbac6cd7f1e830897b0f3b6f5052830601458e27
Belu <cha.maria.belen@gmail.com>
2023-03-21 19:00:21 -0300

    Change

--- config.rb
+++ config.rb
@@ -1,9 +1,9 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
-end
+# activate :autoprefixer do |prefix|
+#   prefix.browsers = "last 2 versions"
+# end
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
@@ -14,10 +14,13 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 
 configure :build do
-  activate :gh_pages
+  # activate :gh_pages
 
   # Append a hash to asset urls (make sure to use the url helpers)
   activate :asset_hash
+  set :css_dir, 'stylesheets'
+  set :js_dir, 'javascripts'
+  set :images_dir, 'images'
 
   # Include the .npmrc file in the build directory
   FileUtils.cp('.npmrc', 'build/')
