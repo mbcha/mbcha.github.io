9d4d6a428f8f476e93ec292fe1208e6f131b7e44
Belu <cha.maria.belen@gmail.com>
2023-03-22 18:49:03 -0300

    Site updated to 51de0cf

--- javascripts/fixContactCard.js
+++ javascripts/fixContactCard.js
@@ -3,9 +3,10 @@ const projectsCard = document.querySelector('.card-projects');
 const contactCardBottomLayer = document.querySelector('.card-contact-bottom-layer');
 
 function fixContactCard() {
-  if (window.pageYOffset > (contactCard.offsetTop - 20) && window.screen.width < 850) { // 20 is the margin
+  // 40 is the margin * 2
+  if (window.pageYOffset > (contactCard.offsetTop - 40) && window.screen.width < 850) {
     contactCard.classList.add('fixed-card');
-    contactCardBottomLayer.style.height = `${contactCard.clientHeight + 40}px`; // 40 is the margin * 2
+    contactCardBottomLayer.style.height = `${contactCard.clientHeight + 40}px`;
     contactCardBottomLayer.style.visibility = 'visible';
   }
   if (window.pageYOffset < projectsCard.offsetTop && window.screen.width < 850) {
