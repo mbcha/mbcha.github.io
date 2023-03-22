0f8b3b1c8fc6e325aa2af77f8652cd614d87c038
Belu <cha.maria.belen@gmail.com>
2023-03-22 13:04:16 -0300

    Add projects links and fix animation

--- config.rb
+++ config.rb
@@ -9,19 +9,7 @@
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+# page '/*.xml', layout: false
+# page '/*.json', layout: false
+# page '/*.txt', layout: false
 
-# configure :build do
-#   # activate :gh_pages
-
-#   # Append a hash to asset urls (make sure to use the url helpers)
-#   activate :asset_hash
-#   set :css_dir, 'stylesheets'
-#   set :js_dir, 'javascripts'
-#   set :images_dir, 'images'
-
-#   # Include the .npmrc file in the build directory
-#   FileUtils.cp('.npmrc', 'build/')
-# end
